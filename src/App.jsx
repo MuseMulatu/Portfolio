59d8e2ca1c76567a72c19391d3336316e434477a
Muse Mulatu <musemulatu.g@gmail.com>
2024-06-29 17:28:39 +0300

    google tracking integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import ReactGA from "react-ga4";
+
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import MemoryGame from "./components/memory-game";
-
+ReactGA.initialize("G-C07ZRV5MDK");
 const App = () => {
+  useEffect(() => {
+    // Track the initial page load
+    ReactGA.pageview(
+      window.location.pathname + window.location.search + window.location.hash
+    );
+
+    // Function to track hash changes
+    const trackPageView = () => {
+      ReactGA.pageview(
+        window.location.pathname + window.location.search + window.location.hash
+      );
+    };
+
+    // Add event listener for hash changes
+    window.addEventListener("hashchange", trackPageView);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("hashchange", trackPageView);
+    };
+  }, []);
   return (
     <>
       <Navbar />
